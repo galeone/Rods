4fe91b88b5e57490a15267f6eef0fb38bb520e82
Paolo Galeone <nessuno@nerdz.eu>
2016-02-12 17:45:46 +0100

    Trying to handle touching rods

--- main.cpp
+++ main.cpp
@@ -113,10 +113,6 @@ void drawAxis(Mat& img,
 
   double hypotenuse = sqrt(static_cast<double>((p.y - q.y) * (p.y - q.y) +
                                                (p.x - q.x) * (p.x - q.x)));
-  //    double degrees = angle * 180 / CV_PI; // convert radians to degrees
-  //    (0-180 range)
-  //    cout << "Degrees: " << abs(degrees - 180) << endl; // angle in 0-360
-  //    degrees range
   // Here we lengthen the arrow by a factor of scale
   q.x = (int)(p.x - scale * hypotenuse * cos(angle));
   q.y = (int)(p.y - scale * hypotenuse * sin(angle));
@@ -158,7 +154,6 @@ int main() {
     // Find connected components of not touching rods
     Mat1b rois;
     vector<Rod> tree = buildTree(bw, rois);
-    size_t treeSize = tree.size();
 
     // Remove every detected rods from the work image
     // thus we can focus on the research of connected rods
@@ -179,8 +174,9 @@ int main() {
     Mat dist;
     bw_work.copyTo(bw_erosed);
 
+    // Separe touching objects
     Mat element = getStructuringElement(MORPH_CROSS, Size(3, 3));
-    morphologyEx(bw_work, bw_erosed, MORPH_ERODE, element, Point(-1, -1), 1);
+    morphologyEx(bw_work, bw_erosed, MORPH_ERODE, element, Point(-1, -1), 2);
 
     // Apply the distance transform to the erosed image to get an image
     // with a distribution of pixel in function their distance
@@ -189,39 +185,60 @@ int main() {
     // Normalize the distance image between [0,1] and threshold it with a
     // threshold of 0.5. Than dilate to make the rods bigger
     normalize(dist, dist, 0, 1, NORM_MINMAX);
+    show(dist);
     threshold(dist, dist, 0.5, 1, CV_THRESH_BINARY);
-    morphologyEx(dist, dist, MORPH_DILATE, element);
+    show(dist);
 
     // Convert the thresholded distance image back to a uchar matrix
     // Invert colour and binarize the image to get object white and non-object
     // black
-    Mat1b dist_8u;
-    dist.convertTo(dist_8u, CV_8UC1);
-    bitwise_not(dist_8u, dist_8u);
-    binarize(dist_8u, dist_8u);
-    // show(dist_8u);
+    Mat dist_8u;
+    dist.convertTo(dist_8u, CV_8U);
 
     // Extract contours of the "objects" (the skeleton of the rods)
     // to get the seeds for the whatershed algoritm
     vector<vector<Point>> contours;
-    findContours(dist_8u, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    findContours(dist_8u, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
     if (contours.size() > 0) {
       // Create the marker image for the watershed algorithm
       Mat markers = Mat::zeros(dist.size(), CV_32SC1);
 
+      // increase the size of every contour, along the major axis direction
+      for (size_t i = 0; i < contours.size(); i++) {
+        // fake rod
+        Rod rod(contours[i], gray.size());
+        vector<Point2d> eigen_vecs = rod.getEigenVectors();
+        vector<double> eigen_val = rod.getEigenValues();
+
+        // p1 shows the direction along the mayor axis. Is the distance scaled
+        // down by a factor of 0.02
+        Point center = rod.getPosition();
+        Point eigenPoint =
+            Point(static_cast<int>(eigen_vecs[0].x * eigen_val[0]),
+                  static_cast<int>(eigen_vecs[0].y * eigen_val[0]));
+
+        Point p1 = center - 0.07 * eigenPoint, p2 = center + 0.04 * eigenPoint;
+        line(dist_8u, center, p1, Scalar(255), 8);
+        line(dist_8u, center, p2, Scalar(255), 8);
+        morphologyEx(dist_8u,dist_8u,MORPH_DILATE,element, Point(-1,-1));
+      }
+
+      findContours(dist_8u, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
       // Draw the foreground markers
       for (size_t i = 0; i < contours.size(); i++) {
-        drawContours(markers, contours, static_cast<int>(i),
-                     Scalar::all(static_cast<int>(i) + 1), CV_FILLED);
+        Scalar color = Scalar::all(static_cast<int>(i) + 1);
+        drawContours(markers, contours, static_cast<int>(i), color, CV_FILLED);
       }
 
       // Draw the background marker
       circle(markers, Point(5, 5), 3, CV_RGB(255, 255, 255), CV_FILLED);
 
+      show(markers * 10000);
+
       // Perform the watershed algorithm
       // convert bw_work in a color image (required by the algorithm)
       Mat color;
-      cvtColor(bw_work, color, CV_GRAY2BGR);
+      cvtColor(gray, color, CV_GRAY2BGR);
       watershed(color, markers);
 
       Mat mark = Mat::zeros(markers.size(), CV_8UC1);
@@ -267,7 +284,6 @@ int main() {
 
       // Draw a straight line perpendicular to the major axis and passing
       // throught the barycenter
-
       vector<Point2d> eigen_vecs = rod.getEigenVectors();
       vector<double> eigen_val = rod.getEigenValues();
 
@@ -294,8 +310,7 @@ int main() {
       // drawAxis(rois, center, p2, Scalar(255), 5);
     }
 
-    // show(bw_work, string(image + string("a")));
-    show(rois, string(image));
+    // show(rois, string(image));
   }
 
   waitKey(0);
