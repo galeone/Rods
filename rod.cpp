3d94482a7be00b2e351830e2fbd10185a0dc203b
Paolo Galeone <nessuno@nerdz.eu>
2016-05-28 12:04:12 +0200

    Add MPL

--- rod.cpp
+++ rod.cpp
@@ -1,3 +1,15 @@
+/*
+Rods: visual inspection of motorcycle connecting rods improving the
+result of the watershed algorithm exploiting the domain knowledge
+Copyright (C) 2016 Paolo Galeone <nessuno@nerdz.eu>
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+Exhibit B is not attached; this software is compatible with the
+licenses expressed under Section 1.12 of the MPL v2.
+*/
+
 #include "rod.h"
 
 Rod::Rod(vector<Point>& contour, Size size) {
